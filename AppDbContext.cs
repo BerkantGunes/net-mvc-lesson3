c1098e72960775765180dceb5e42831876dc48d3
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-12 17:57:37 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CAR.ADMIN.Models
+{
+   
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Ogrenci> Ogrenci { get; set; }
+        public AppDbContext()
+            :base("name=DefaultConnection")
+        {
+            
+        }
+    }
+}
---
+++ Ogrenci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CAR.ADMIN.Models
+{
+    [Table("Ogrenci")]
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string adSoyad { get; set; }
+        public string Adres { get; set; }
+        public string eMail { get; set; }
+    }
+}
