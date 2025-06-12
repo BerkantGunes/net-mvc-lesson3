d5ea4a670edbd9ec19c2c62778e1fb7a97d97fba
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-12 17:57:56 +0300

    Add files via upload

---
+++ BasicTablesController.cs
@@ -0,0 +1,20 @@
+﻿using CAR.ADMIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CAR.ADMIN.Controllers
+{
+    public class BasicTablesController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+        // GET: BasicTables
+        public ActionResult Index()
+        {
+            var ogrenciler = db.Ogrenci.ToList();
+            return View(ogrenciler);
+        }
+    }
+}
